a28c39470150fb01643e71b35283c9a9af5f2ba4
Gift <gkenny896@gmail.com>
2021-03-09 07:53:37 +0100

    Added Strigs

--- test.rb
+++ test.rb
@@ -1,2 +1,18 @@
-x = 9
-puts x
+# puts "Hello, run.rb!"
+
+# x = 'Hello '
+# x += 'Gift'
+# puts x
+
+#Maths in ruby
+# a = 2
+# b = 7
+# puts a + b
+# puts a * b
+# puts a%b
+# puts a/b
+# puts b**a
+
+# Strings
+helloWorld = "Hello world. This is Gifts greeting to yall"
+puts helloWorld
